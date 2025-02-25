be2104f5b8b4e6cf557159d0a5723799174ae893
yostane <1958676+yostane@users.noreply.github.com>
2025-02-25 12:18:55 +0100

    Fix equality method in Person class and add test instances for validation

--- material/python/poo/ex_person.py
+++ material/python/poo/ex_person.py
@@ -8,6 +8,18 @@ class Person:
     return f"Person(name: {self.first_name} {self.last_name}, age: {self.age})"
   
   def __eq__(self, other: object) -> bool:
-    if not isinstance(other, Person):
-      return False
-    return self.first_name == other.age and self.last_name == other.last_name and self.age == other.age
+    return isinstance(other, Person) and self.first_name == other.first_name\
+      and self.last_name == other.last_name and self.age == other.age
+  
+
+p1 = Person("Ram", "Bo", 40)
+p2 = Person("Ryuuji", "Itadori", 17)
+p3 = Person("Ryuuji", "Itadori", 17)
+
+print(p1)
+print(p2)
+print(p3)
+
+print(p1 == 10)
+print(p1 == p2)
+print(p2 == p3)
