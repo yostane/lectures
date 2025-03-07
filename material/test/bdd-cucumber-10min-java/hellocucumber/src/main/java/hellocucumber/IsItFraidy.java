01ce3bba759005aed92dcd379de8be02d1096d0f
yostane <1958676+yostane@users.noreply.github.com>
2025-03-07 12:07:49 +0100

    Refactor BDD Cucumber setup for Java: add IsItFraidy class and update feature scenarios

---
+++ material/test/bdd-cucumber-10min-java/hellocucumber/src/main/java/hellocucumber/IsItFraidy.java
@@ -0,0 +1,7 @@
+package hellocucumber;
+
+class IsItFraidy {
+  static String check(String day) {
+    return "Friday".equals(day) ? "TGIF" : "Nope";
+  }
+}
--- material/test/bdd-cucumber-10min-java/hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ material/test/bdd-cucumber-10min-java/hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -2,34 +2,26 @@ package hellocucumber;
 
 import io.cucumber.java.en.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class IsItFraidy {
-  static String check(String day) {
-    return null;
-  }
-}
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StepDefinitions {
 
   private String today;
   private String actualAnswer;
 
-  @Given("today is Sunday")
-  public void today_is_sunday() {
-    this.today = "Sunday";
+  @Given("today is {string}")
+  public void today_is_sunday(String day) {
+    this.today = day;
   }
 
   @When("I ask whether it's Friday yet")
   public void i_ask_whether_it_s_friday_yet() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    this.actualAnswer = IsItFraidy.check(this.today);
   }
 
   @Then("I should be told {string}")
-  public void i_should_be_told(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  public void i_should_be_told(String expectedAnwser) {
+    assertThat(actualAnswer).isEqualTo(expectedAnwser);
   }
 
 }
