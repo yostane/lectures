c78428dfde9ca35df9812878f710bbcb3011e34c
yostane <1958676+yostane@users.noreply.github.com>
2025-03-14 11:21:45 +0100

    Add aggregation, composition, and association concepts to documentation and implement related classes

--- docs/python/python-poo.md
+++ docs/python/python-poo.md
@@ -34,6 +34,18 @@ poo_2.py
 --8<--
 ```
 
+## Agrégation, composition et association
+
+- L'agrégation est une relation entre deux classes où une classe contient une référence vers une autre classe.
+- La composition est une relation plus forte que l'agrégation. Dans ce cas, la classe enfant ne peut pas exister sans la classe parente.
+- L'association est une relation entre deux classes où une classe utilise une autre classe sans la contenir.
+
+```py
+--8<--
+poo_07_relations.py
+--8<--
+```
+
 ## UML
 
 - UML (Unified Modeling Language) est un langage de modélisation graphique pour la conception de logiciels.
---
+++ material/python/poo/poo_07_relations.py
@@ -0,0 +1,43 @@
+class Salary:
+    def __init__(self, pay):
+        self.pay = pay
+
+    def get_total(self):
+        return self.pay * 12
+
+
+class Car:
+    pass
+
+
+class House:
+    pass
+
+
+class Computer:
+    def turn_on(self):
+        print("Turning on")
+
+
+class Employee:
+    def __init__(self, pay, bonus, posessions):
+        self.bonus = bonus
+        # Composition
+        self.salary = Salary(pay)
+        # Agrégation
+        self.posessions = posessions
+
+    def compute_annual_salary(self):
+        return self.salary.get_total() + self.bonus
+
+    def work(self, computer):
+        computer.turn_on()
+
+
+c1 = Car()
+c2 = Car()
+h = House()
+emp = Employee(100, 10, [c1, c2, h])
+print(emp.compute_annual_salary())
+comp = Computer()
+emp.work(comp)
