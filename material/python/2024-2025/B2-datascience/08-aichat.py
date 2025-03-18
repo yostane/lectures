d6681726d07f262f9885d21032ea93ab8a58ca3d
yostane <1958676+yostane@users.noreply.github.com>
2025-03-18 17:14:11 +0100

    Update AI assistant functionality to analyze weather conditions based on humidity and temperature inputs

--- material/python/2024-2025/B2-datascience/08-aichat.py
+++ material/python/2024-2025/B2-datascience/08-aichat.py
@@ -2,7 +2,11 @@ import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 system_message = (
-    "system", "You are an assistant that translated Eglish to French. Translate the user sentence")
+    "system", """You are a weather specialist. 
+    The human provides humidity and temperature. 
+    Can you explain the results and comment on the air condition in the house.
+    Please provide a short answer in two sentences.
+    """)
 googleai_api_key = st.sidebar.text_input("Google AI API Key", type="password")
 
 # Arrêter l'application streamlit si pas de clé d'API
@@ -10,7 +14,7 @@ if len(googleai_api_key) == 0:
     st.error("No API key")
     st.stop()
 
-st.title("Traduction avec IA")
+st.title("AI weather analyst")
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash-lite", api_key=googleai_api_key)
 
