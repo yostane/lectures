593b49381f3422435a605fbd30f211afa08c87d6
yostane <1958676+yostane@users.noreply.github.com>
2025-03-21 10:47:16 +0100

    Add Triangle class to GeometricShapes with perimeter and area methods

--- material/python/poo/GeometricShapes.py
+++ material/python/poo/GeometricShapes.py
@@ -32,7 +32,23 @@ class Circle(Shape):
     
     def perimeter(self) -> float:
         return self.radius * 2 * math.pi
+    
+class Triangle(Shape):
+    """On suppose qu'on donne une base et une hauteur ainsi que les deux autres côté.
+    A faire en exercice: se baser uniquement sur les côtés"""
+    def __init__(self, b: float, h: float, c2: float, c3: float):
+        super().__init__()
+        self.b = b
+        self.h = h
+        self.c2 = c2
+        self.c3 = c3
+    
+    def perimeter(self) -> float:
+        return self.b + self.c2 + self.c3
+    
+    def area(self) -> float:
+        return (self.b * self.h) / 2
 
-shapes = (Rectangle(10, 2.5), Rectangle(9, 3), Circle(9), Circle(1.8))
+shapes = (Rectangle(10, 2.5), Rectangle(9, 3), Circle(9), Circle(1.8), Triangle(10, 10, 10, 14.142))
 for shape in shapes:
     print("area", shape.area(), "perimeter", shape.perimeter())
