ca7be828915ac6d5898a526a0071b627b8ab7a94
yostane <1958676+yostane@users.noreply.github.com>
2025-03-31 10:32:16 +0200

    Implement Bowling score calculator and add comprehensive test cases

--- material/test/tdd-bowling-kata/src/bowling.ts
+++ material/test/tdd-bowling-kata/src/bowling.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+export class BowlingCalcultor {
+  static getScore(frames: number[]): number {
+    let sum = 0;
+    for (let index = 0; index < frames.length; index++) {
+      const frame = frames[index];
+      const nextFrame = frames[index + 1];
+      if (frame === 10) {
+        sum += 10 + nextFrame + frames[index + 2];
+      } else if (index < 19) {
+        sum += frame;
+      } else if (frame + nextFrame === 10) {
+        sum += frame + nextFrame + frames[index + 2] * 2;
+      }
+    }
+    return sum;
+  }
+}
--- material/test/tdd-bowling-kata/tests/bowling.test.ts
+++ material/test/tdd-bowling-kata/tests/bowling.test.ts
@@ -1,12 +1,49 @@
-import { describe, expect, it } from "bun:test";
+import { describe, expect, it, test, type Test } from "bun:test";
+import { BowlingCalcultor } from "../src/bowling";
 
-/*
-describe("sum function", () => {
-  it("should return the sum of two numbers (both are positive)", () => {
-    expect(sum(2, 3)).toBe(5);
-  });
-  it("should return the sum of two numbers (one is negative)", () => {
-    expect(sum(-1, 2)).toBe(1);
-  });
+interface TestCase {
+  description: string;
+  rolls: number[];
+  expectedScore: number;
+}
+
+const testCases: TestCase[] = [
+  {
+    description: "Should get 0 is all frames are 0",
+    rolls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    expectedScore: 0,
+  },
+  {
+    description:
+      "Should compute the toal score when there are no spares not strikes",
+    rolls: [3, 4, 5, 2, 3, 2, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    expectedScore: 27,
+  },
+  {
+    description:
+      "Should compute the toal score when there is one only strike and no spares",
+    rolls: [10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    expectedScore: 24,
+  },
+  {
+    description:
+      "Should compute the toal score when there is one only strike and the end and no spares",
+    rolls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 1, 2],
+    expectedScore: 13,
+  },
+  {
+    description:
+      "Should compute the toal score when there is one only spare at the beginning and no strike",
+    rolls: [5, 5, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    expectedScore: 24,
+  },
+];
+
+describe("Bowling score calculator", () => {
+  for (const testCase of testCases) {
+    it(testCase.description, () => {
+      const actualScore = BowlingCalcultor.getScore(testCase.rolls);
+      expect(actualScore).toBe(testCase.expectedScore);
+    });
+  }
 });
-*/
