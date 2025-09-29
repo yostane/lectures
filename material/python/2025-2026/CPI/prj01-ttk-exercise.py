090de5d1b455e6cff93a05baa6f5b9c1bec91c0a
yostane <1958676+yostane@users.noreply.github.com>
2025-09-29 16:50:22 +0200

    Update docstring to clarify output details in tkinter application

--- material/python/2025-2026/CPI/prj01-ttk-exercise.py
+++ material/python/2025-2026/CPI/prj01-ttk-exercise.py
@@ -1,9 +1,9 @@
 """Create a tkinter application with a button and one or many labels.
 Every time the user presses on the button, generate a random integer between 3 and 24.
-Then show this information in the window: 
-- the random number
+Then, show this information in the window: 
+- The last generated random number
 - If it's even or odd 
-- If it's prime or not
+- If it's prime or not (you can resuse existing exercises)
 - The list of all generated random numbers
 - The sum of all generated random numbers
 """
