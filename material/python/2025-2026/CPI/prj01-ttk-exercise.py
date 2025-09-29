a6be77ffeb2e6e8ddf2aa47462bd0a530f4dbdb0
yostane <1958676+yostane@users.noreply.github.com>
2025-09-29 17:31:05 +0200

    Refactor tkinter application to include random number generation and display logic

--- material/python/2025-2026/CPI/prj01-ttk-exercise.py
+++ material/python/2025-2026/CPI/prj01-ttk-exercise.py
@@ -7,3 +7,39 @@ Then, show this information in the window:
 - The list of all generated random numbers
 - The sum of all generated random numbers
 """
+import tkinter as tk
+from tkinter import ttk
+import random
+
+root = tk.Tk()
+label = ttk.Label(root, text='Please press on the button')
+
+random_values = []
+
+
+def get_sum(values):
+    s = 0
+    for x in values:
+        s += x
+    return s
+
+
+def generate_random_value():
+    r = random.randint(3, 24)
+    random_values.append(r)
+    even_value = "odd"
+    if r % 2 == 0:
+        even_value = "even"
+    label["text"] = f"""
+    - Last random value: {r}
+    - random values: {random_values}
+    - sum {get_sum(random_values)}
+    - {even_value}
+    """
+
+
+button = ttk.Button(root, text="Change label", command=generate_random_value)
+
+label.pack()
+button.pack()
+root.mainloop()
