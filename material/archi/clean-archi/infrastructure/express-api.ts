2948efb79707610a6c7eb071e2b2332cc2f77b0d
yostane <1958676+yostane@users.noreply.github.com>
2025-11-24 12:22:45 +0100

    fix: update express API to use async/await for memberController methods

--- material/archi/clean-archi/infrastructure/express-api.ts
+++ material/archi/clean-archi/infrastructure/express-api.ts
@@ -4,27 +4,27 @@ import { Member } from "business-domain";
 import express from "express";
 import bodyParser from "body-parser";
 
-const app = express();
-app.use(bodyParser.json());
-
 const sqliteMemberRepository = new SqliteMemberRepository();
 await sqliteMemberRepository.setup();
 const memberController = new MemberController(sqliteMemberRepository);
 
-app.get("/", (req, res) => {
-  const members = memberController.getAll();
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", async (req, res) => {
+  const members = await memberController.getAll();
   res.send(members);
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const member: Member = req.body;
-  memberController.add(member);
+  await memberController.add(member);
   res.sendStatus(201);
 });
 
-app.delete("/:id", (req, res) => {
+app.delete("/:id", async (req, res) => {
   const memberId = +req.params.id;
-  memberController.delete(memberId);
+  await memberController.delete(memberId);
   res.sendStatus(200);
 });
 
