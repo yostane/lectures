dd658de56488b6a7e6568d55e3ecfe85e59b7c8c
yostane <1958676+yostane@users.noreply.github.com>
2026-04-02 12:34:12 +0200

    feat: add Supabase integration and implement task loading in Home view

--- material/desktopdev/25-26/electron-react-app-ts-2/src/views/Home.tsx
+++ material/desktopdev/25-26/electron-react-app-ts-2/src/views/Home.tsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabase";
+import type { Task } from "../model/Task";
+
 export default function Home() {
+  const [tasks, setTasks] = useState(Array<Task>());
+
+  useEffect(() => {
+    async function loadTasks() {
+      const selectResult = await supabase.from("tasks").select();
+      console.log("select", selectResult);
+      const tasks = selectResult.data;
+      console.log("tasks", tasks);
+      if (tasks) {
+        setTasks(tasks);
+      }
+    }
+
+    loadTasks();
+  }, []);
+
   return (
-    <div className="home">
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is the main landing page of our application.</p>
-    </div>
+    <>
+      <h1>My tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.title}>{task.content}</li>
+        ))}
+      </ul>
+    </>
   );
 }
