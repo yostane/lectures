ec720e91c9fd3cb266bc8f7c7abc4db4e59c93f9
yostane <1958676+yostane@users.noreply.github.com>
2026-04-02 11:30:26 +0200

    feat: integrate MUI components and update About view with tabs
    - Added MUI dependencies for styling and components.
    - Refactored App component to use MUI Grid and Stack for layout.
    - Simplified App.css by removing unused styles.
    - Enhanced About view with MUI Tabs for better navigation and content organization.

--- material/desktopdev/25-26/electron-react-app-ts-2/src/App.tsx
+++ material/desktopdev/25-26/electron-react-app-ts-2/src/App.tsx
@@ -1,14 +1,22 @@
 import { Link, Outlet } from "react-router-dom";
 import "./App.css";
+import { Button, Grid, Stack } from "@mui/material";
 
 function App() {
   return (
     <>
-      <div>
-        <Link to="/home">Accueil</Link> -<Link to="/about">A propos</Link> -
-        <Link to="/contact">Contact</Link>
-      </div>
-      <Outlet />
+      <Grid container spacing={2}>
+        <Grid size={4}>
+          <Stack spacing={2}>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </Stack>
+        </Grid>
+        <Grid size={8}>
+          <Outlet />
+        </Grid>
+      </Grid>
     </>
   );
 }
