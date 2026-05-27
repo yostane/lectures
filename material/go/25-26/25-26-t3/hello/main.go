03f00319df294f5de02d06e373e2add075cfde6f
yostane <1958676+yostane@users.noreply.github.com>
2026-05-27 18:56:01 +0200

    Add initial implementation of CLI application `univ-cli` with file system and TUI commands

---
+++ material/go/25-26/25-26-t3/hello/main.go
@@ -0,0 +1,9 @@
+package main // package qui contient la fonction main
+
+import "fmt"
+
+// Cette fonction sera exécutée par le runtime automatiquement
+// Elle est appelée point d'entrée
+func main() {
+	fmt.Println("Hello Go 🎉")
+}
