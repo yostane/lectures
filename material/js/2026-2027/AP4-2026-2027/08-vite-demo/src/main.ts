66f4f5b1a47723b238c168004c36d125b454d96c
yostane <1958676+yostane@users.noreply.github.com>
2026-09-11 15:34:53 +0200

    feat: restructure HTML layout and enhance date display functionality in main script

--- material/js/2026-2027/AP4-2026-2027/08-vite-demo/src/main.ts
+++ material/js/2026-2027/AP4-2026-2027/08-vite-demo/src/main.ts
@@ -1,13 +1,35 @@
 import "./style.css";
 
+const state = {
+  dates: [],
+};
+
+const handler = {
+  set(obj, prop, value) {
+    const app = document.querySelector<HTMLDivElement>("#app");
+    if (!app) {
+      return true;
+    }
+    const dates = value as Date[];
+    app.innerHTML = "";
+    for (const date of dates) {
+      const p = document.createElement("p") as HTMLParagraphElement;
+      p.innerText = `- Il est ${date.toISOString()}`;
+      app?.appendChild(p);
+    }
+    console.log(value);
+    obj[prop] = value;
+    return true;
+  },
+};
+const proxy = new Proxy(state, handler);
+
 // DOMContentLoaded permet de s'assurer que tout le DOM est bien chargé côté JS
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector<HTMLButtonElement>("#add-item");
   const app = document.querySelector<HTMLDivElement>("#app");
   button?.addEventListener("click", (e) => {
-    const p = document.createElement("p");
-    const content = document.createTextNode(`- Il est ${new Date()}`);
-    p.appendChild(content);
-    app?.appendChild(p);
+    const newDates = [...state.dates, new Date()];
+    proxy.dates = newDates;
   });
 });
